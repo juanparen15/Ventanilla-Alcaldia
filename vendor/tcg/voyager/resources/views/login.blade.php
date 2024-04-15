1298bacb39f9ce905738921e907ae2f4543c1606
juanparen15 <jprendon9@gmail.com>
2024-04-15 18:35:39 -0500

    vendor

---
+++ vendor/tcg/voyager/resources/views/login.blade.php
@@ -0,0 +1,75 @@
+@extends('voyager::auth.master')
+@section('content')
+<a class="navbar-brand" href="{{ route('voyager.dashboard') }}">
+    <div class="logo-icon-container">
+        <?php $admin_logo_img = Voyager::setting('admin.icon_image', ''); ?>
+        @if($admin_logo_img == '')
+            <img src="{{ voyager_asset('images/logo-icon-light.png') }}" alt="Logo Icon">
+        @else
+            <img src="{{ Voyager::image($admin_logo_img) }}" alt="Logo Icon">
+        @endif
+    </div>
+</a>
+    <div class="login-container">
+        <p>{{ __('voyager::login.signin_below') }}</p>
+
+        <x-guest-layout>
+            <x-authentication-card>
+                {{-- <x-slot name="logo">
+                    <x-authentication-card-logo />
+                </x-slot> --}}
+
+                <x-validation-errors class="mb-4" />
+
+                @if (session('status'))
+                    <div class="mb-4 font-medium text-sm text-green-600">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <div>
+                        <x-label for="username" value="{{ __('Numero de Identificación') }}" />
+                        <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')"
+                            required autofocus autocomplete="username" />
+                    </div>
+
+                    <div class="mt-4">
+                        <x-label for="password" value="{{ __('Contraseña') }}" />
+                        <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                            autocomplete="current-password" />
+                    </div>
+
+                    <div class="block mt-4">
+                        <label for="remember_me" class=" items-center">
+                            <x-checkbox id="remember_me" name="remember" />
+                            <span class="ml-2 text-sm text-gray-600">{{ __('Recuerdame') }}</span>
+                        </label>
+                    </div>
+                    <x-button class="items-center justify-end mt-4">
+                        {{ __('Iniciar Sesión') }}
+                    </x-button>
+                    
+                    <div class="items-center justify-end mt-4">
+                        @if (Route::has('password.request'))
+                            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                href="{{ route('password.request') }}">
+                                {{ __('¿Olvidaste tu contraseña?') }}
+                            </a>
+                        @endif
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}"
+                                class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Registrarme</a>
+                        @endif
+
+
+                    </div>
+
+                </form>
+            </x-authentication-card>
+        </x-guest-layout>
+    </div> <!-- .login-container -->
+@endsection

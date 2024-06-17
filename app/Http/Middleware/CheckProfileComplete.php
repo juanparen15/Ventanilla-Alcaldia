c515bb8208ea97cc1539e5d55b5b6705c571bc03
juanparen15 <jprendon9@gmail.com>
2024-06-16 21:06:21 -0500

    Agregacion de Middleware para obligacion de rellenar informacion de perfil

---
+++ app/Http/Middleware/CheckProfileComplete.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class CheckProfileComplete
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+
+        // Obtener el nombre de la ruta actual
+        $currentRoute = $request->route()->getName();
+
+        // Permitir solicitudes AJAX y la página de edición de perfil
+        if (
+            $user && $user->hasVerifiedEmail() && !$this->profileCompleted($user) &&
+            !$request->ajax() &&
+            $currentRoute !== 'voyager.users.edit' &&
+            $currentRoute !== 'voyager.users.update' // Permitir también la actualización del perfil
+        ) {
+            // Obtiene el ID del usuario autenticado
+            $userId = Auth::id();
+
+            // Redireccionar a la página de edición del usuario autenticado
+            return redirect()->route('voyager.users.edit', $userId)->with('message', 'Por favor, completa tu perfil antes de continuar.');
+        }
+
+        return $next($request);
+    }
+
+    private function profileCompleted($user)
+    {
+        // Lógica para verificar si el perfil del usuario está completo
+        // Verificar cada campo requerido
+        return !is_null($user->primer_nombre) &&
+            !is_null($user->primer_apellido);
+            !is_null($user->movil) &&
+            !is_null($user->direccion) &&
+            !is_null($user->fecha_nacimiento) &&
+            !is_null($user->peso) &&
+            !is_null($user->altura) &&
+            !is_null($user->entrenador_personal) &&
+            !is_null($user->entrenador_nacional) &&
+            !is_null($user->inicio_competencia) &&
+            !is_null($user->practicando_tiro_desde) &&
+            !is_null($user->medico_tramitante) &&
+            !is_null($user->lugar_entrenamiento) &&
+            !is_null($user->estado_civil) &&
+            !is_null($user->genero);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
     return view('home');
 });
 
-
+// Rutas para la verificación de correo electrónico
 Route::get('/email/verify', function () {
     return view('auth.verify-email');
 })->middleware('auth')->name('verification.notice');
@@ -35,100 +35,19 @@ Route::post('/email/verification-notification', 'App\Http\Controllers\Auth\Verif
     ->middleware(['auth', 'throttle:6,1'])
     ->name('verification.send');
 
+// Ruta para el panel de administración solo si el usuario está autenticado y verificado
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+    // Ruta al panel de administración
     Route::get('/admin', function () {
         return view('dashboard');
     })->name('dashboard');
 });
 
+// Rutas de Voyager
 Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'verified']], function () {
     Voyager::routes();
 });
+
 Route::get('/get-municipios/{departamento}', 'TCG\Voyager\Http\Controllers\AjaxController@obtener_municipios');
 Route::get('/get-club/{liga}', 'TCG\Voyager\Http\Controllers\AjaxController@obtener_club');
 Route::get('/get-modalidades/{tipo_arma}', 'TCG\Voyager\Http\Controllers\AjaxController@obtener_modalidades');
-
-
-// Manejo de excepciones error 419 page expired
-// Route::get('/test-419', function () {
-//     throw new \Illuminate\Session\TokenMismatchException;
-// });
-
-
-
-
-// <?php
-
-// use App\Http\Controllers\Voyager\TipoPeticionController;
-// use Illuminate\Support\Facades\Notification;
-// use Illuminate\Foundation\Auth\EmailVerificationRequest;
-// use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\Route;
-
-// /*
-// |--------------------------------------------------------------------------
-// | Web Routes
-// |--------------------------------------------------------------------------
-// |
-// | Here is where you can register web routes for your application. These
-// | routes are loaded by the RouteServiceProvider and all of them will
-// | be assigned to the "web" middleware group. Make something great!
-// |
-// */
-
-// Route::get('/', function () {
-//     // return view('welcome');
-//     return view('home');
-// });
-
-
-// Route::middleware([
-//     'auth:sanctum',
-//     'verified',
-// ])->group(function () {
-
-//     // Route::get('/', ['uses' => $namespacePrefix . 'VoyagerController@index',   'as' => 'dashboard']);
-//     Route::get('/', function () {
-//         return view('home');
-//     })->name('home');
-// });
-
-// Route::get('/email/verify', function () {
-//     return view('auth.verify-email');
-// })->middleware('auth')->name('verification.notice');
-
-// Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-//     $request->fulfill();
-
-//     return redirect('/home');
-// })->middleware(['auth', 'signed'])->name('verification.verify');
-
-
-// Route::post('/email/verification-notification', function (Request $request) {
-//     $request->user()->sendEmailVerificationNotification();
-
-//     return back()->with('message', 'Verification link sent!');
-// })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-
-
-// // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-// //     return view('dashboard');
-// // })->name('dashboard');
-
-// Route::group(['prefix' => 'admin'], function () {
-//     Voyager::routes();
-// });
-
-// Route::get('/admin', function () {
-//     // Only verified users may access this route...
-// })->middleware(['auth', 'verified']);
-
-// Route::get('/get-municipios/{departamento}', 'TCG\Voyager\Http\Controllers\AjaxController@obtener_municipios');
-// Route::get('/get-club/{liga}', 'TCG\Voyager\Http\Controllers\AjaxController@obtener_club');
-// Route::get('/get-modalidades/{tipo_arma}', 'TCG\Voyager\Http\Controllers\AjaxController@obtener_modalidades');
-
-
-// // Manejo de excepciones error 419 page expired
-// // Route::get('/test-419', function () {
-// //     throw new \Illuminate\Session\TokenMismatchException;
-// // });
